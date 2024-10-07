1be2981d1bccb42e173c35637ff80f642f95e942
ProgAbdo99 <ggndj80@gmail.com>
2024-10-07 14:28:31 +0300

    Add files via upload

---
+++ Vilta Database Access/clsConnectionSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vilta_Database_Access
+{
+    public class clsConnectionSettings
+    {
+        public static string ConnectionString = "Server=.;Database=Vilta;User Id=sa;Password=sa123456";
+    }
+}
